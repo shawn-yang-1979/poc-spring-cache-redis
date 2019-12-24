45ffc32fb8aa3434e95f1fcc6495bee7892614e2
shawn.yang <shawn.yang@gogoro.com>
2019-12-24 17:24:40 +0800

    t

--- demo-1/src/main/java/com/example/demo/AppRunner.java
+++ demo-1/src/main/java/com/example/demo/AppRunner.java
@@ -9,17 +9,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class AppRunner implements CommandLineRunner {
+    /**
+     * Test
+     */
+    @Autowired
+    private ScooterService scooterService;
 
-	@Autowired
-	private ScooterService scooterService;
-
-	@Override
-	public void run(String... args) throws Exception {
-		this.scooterService.createScooter(ObjectMother.newScooterDetail("G1", "Shawn"));
-		this.scooterService.createScooter(ObjectMother.newScooterDetail("G1", "Basil"));
-		this.scooterService.createScooter(ObjectMother.newScooterDetail("G2", "Jake"));
-		this.scooterService.getScooters().stream().forEach(dto -> log.info(dto.toString()));
-		this.scooterService.getScootersByModel("G1").stream().forEach(dto -> log.info(dto.toString()));
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        this.scooterService.createScooter(ObjectMother.newScooterDetail("G1", "Shawn"));
+        this.scooterService.createScooter(ObjectMother.newScooterDetail("G1", "Basil"));
+        this.scooterService.createScooter(ObjectMother.newScooterDetail("G2", "Jake"));
+        this.scooterService.getScooters().stream().forEach(dto -> log.info(dto.toString()));
+        this.scooterService.getScootersByModel("G1").stream().forEach(dto -> log.info(dto.toString()));
+    }
 
 }
